307195559858ab861afe3f8cfbe387799e621568
WHB901 <abdulwhbhussain@gmail.com>
2023-12-01 01:38:17 +0500

    Add users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
   # This File is Subject to Change as the App is Developed
 
-  # devise token auth to be implemented later using unique username only
+  # devise auth if we have to implement
 
   has_many :reservations, dependent: :destroy # only works if reservation model setup accurately.
   has_many :workspaces, through: :reservations # only works if the workspace model setup accurately.
+
+  has_secure_token :authentication_token
+  # when a user is created, a token is created for them in the database
 
   # username must be unique and present in order to create a user and not null
   validates :username, presence: true, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_191905) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_202047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_30_191905) do
     t.string "username", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "authentication_token"
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
