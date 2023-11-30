8264c24016c6e7c8ecac300e6945fe965d3c6cb7
Javier <javidx@gmail.com>
2023-11-30 13:34:08 +0200

    Modified Reservation model to work with dummy data for testing, waiting for User and Workspace models to be created in order to create a migration referecing their models

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,5 @@
 class Reservation < ApplicationRecord
-  belongs_to :user
-  belongs_to :workspace
+  # To enable when the user and workspace models are created
+  # belongs_to :user
+  # belongs_to :workspace
 end
--- db/migrate/20231130111725_create_reservations.rb
+++ db/migrate/20231130111725_create_reservations.rb
@@ -4,8 +4,8 @@ class CreateReservations < ActiveRecord::Migration[7.1]
       t.string :city
       t.date :start_date
       t.date :end_date
-      t.references :user, null: false, foreign_key: true, index: true
-      t.references :workspace, null: false, foreign_key: true, index: true
+      t.integer :user
+      t.integer :workspace
 
       t.timestamps
     end
