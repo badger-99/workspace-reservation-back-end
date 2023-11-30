c16d3624ac74c5014d01b781e71573289255e8b3
Javier <javidx@gmail.com>
2023-11-30 13:44:20 +0200

    Added migration to prevent null values on reservations

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_30_111725) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_30_114002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "reservations", force: :cascade do |t|
-    t.string "city"
-    t.date "start_date"
-    t.date "end_date"
-    t.integer "user"
-    t.integer "workspace"
+    t.string "city", null: false
+    t.date "start_date", null: false
+    t.date "end_date", null: false
+    t.integer "user", null: false
+    t.integer "workspace", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
