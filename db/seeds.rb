67acc5a935b930ed27cc76b09de36cbd384305e2
WHB901 <abdulwhbhussain@gmail.com>
2023-12-01 00:36:55 +0500

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Create a user
+
+User.create(username: "testuser1")
+
+User.create(username: "testuser2")
+
+User.create(username: "testuser3")
+
+# Add a workspace
+
+# Workspace.create(name: "testworkspace1", location: "testlocation1", capacity: 1)
+
+# Workspace.create(name: "testworkspace2", location: "testlocation2", capacity: 2)
+
+# Add a reservation
+
+# Reservation.create(user_id: 1, workspace_id: 1, start_time: "2021-10-01 10:00:00", end_time: "2021-10-01 11:00:00")
+
+# Reservation.create(user_id: 2, workspace_id: 2, start_time: "2021-10-01 10:00:00", end_time: "2021-10-01 11:00:00")
