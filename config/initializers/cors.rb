cea6e118597b9cc901362fafdd339fc9df2c6663
WHB901 <abdulwhbhussain@gmail.com>
2023-11-30 16:53:27 +0500

    Add cors setup

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,14 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+# config/initializers/cors.rb
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins 'http://localhost:3001' # Replace this with your React frontend's origin or you can use '*' to allow all origins and for production you should add your frontend's origin next to the localhost one.
+
+    resource '/api/v1/*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+  end
+end
