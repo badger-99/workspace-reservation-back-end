519c6769a32736a40b65089d84a1391120b03753
Alfred M <alfred.mkg@proton.me>
2023-12-01 21:08:56 +0300

    Move controlers to app/controllers/api/v1

--- app/controllers/api/v1/workspaces_controller.rb
+++ app/controllers/api/v1/workspaces_controller.rb
@@ -13,7 +13,6 @@ class WorkspacesController < ApplicationController
 
   def destroy
     @deleted_workspace = Workspace.find(params[:workspace_id])
-    # find all reservations with a foreign key similar to this Id and destroy them, then ...
     @deleted_workspace.destroy
   end
 
