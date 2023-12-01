1465e41e0f3a6ea26aa5bb73003fac9467d046c2
Alfred M <alfred.mkg@proton.me>
2023-12-01 19:49:25 +0300

    Update workspace model

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -3,5 +3,5 @@ class Workspace < ApplicationRecord
   validates :description, presence: true, allow_blank: false
   validates :image, presence: true, allow_blank: false
 
-  has_many :reservations
+  has_many :reservations, dependent: :destroy
 end
