59aa34c8189879065ed0d2dee0205dc9dbe8171f
Javier <javidx@gmail.com>
2023-12-01 09:16:27 +0200

    Added validations on reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,9 @@ class Reservation < ApplicationRecord
   # To enable when the user and workspace models are created
   # belongs_to :user
   # belongs_to :workspace
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :city, presence: true
+  validates :workspace, presence: true
+  validates :user, presence: true
 end
