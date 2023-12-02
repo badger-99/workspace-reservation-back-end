aeb1d6aa18f306cbaeaf7076726898344e024274
Alfred M <alfred.mkg@proton.me>
2023-12-03 01:22:21 +0300

    Add image_url route helper in the model

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -8,6 +8,10 @@ class Workspace < ApplicationRecord
 
   validate :image_presence
 
+  def image_url
+    Rails.application.routes.url_helpers.url_for(image) if image.attached?
+  end
+
   private
 
   def image_presence
