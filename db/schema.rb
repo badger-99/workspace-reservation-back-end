4eb679e3116862e4089f29e0cce822247999cb51
Alfred M <alfred.mkg@proton.me>
2023-12-02 18:54:16 +0300

    Remove image column from workspaces table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_01_120745) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_02_134921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -39,7 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_01_120745) do
   create_table "workspaces", force: :cascade do |t|
     t.text "name", null: false
     t.text "description", null: false
-    t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
