4675799fe83b437332a59fb60c9dd9d1b65c258c
Alfred M <alfred.mkg@proton.me>
2023-12-02 21:06:45 +0300

    Add uploaded image association & validation

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,7 +1,16 @@
 class Workspace < ApplicationRecord
   validates :name, presence: true, allow_blank: false
   validates :description, presence: true, allow_blank: false
-  validates :image, presence: true, allow_blank: false
+
+  has_one_attached :image
 
   has_many :reservations, dependent: :destroy
+
+  validate :image_presence
+
+  private
+
+  def image_presence
+    errors.add(:image, 'Image must be uploaded') unless image.attached?
+  end
 end
