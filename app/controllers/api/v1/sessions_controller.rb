07f7732faa61d252f723c1a9afcd3e76adca9d00
zilola <zilolanazarova1994@gmail.com>
2023-12-04 16:44:08 +0500

    Make all controllers inherit from api/v1/application_controller in order to be able to use authenticate_request function from any controller, use strong parameters to permit only allowed parameters, encode user's id when session is created and return a new generated token in a response

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,58 +1,26 @@
 # app/controllers/api/v1/sessions_controller.rb
 
-class Api::V1::SessionsController < ApplicationController
+class Api::V1::SessionsController < Api::V1::ApplicationController
   # POST /api/v1/sessions
-  def create
-    request_data = JSON.parse(request.body.read)
-    @user = User.find_by(username: request_data['username'])
+  skip_before_action :authenticate_request
 
+  def create
+    @user = User.find_by_username(user_params[:username])
     if @user
+      token = jwt_encode(user_id: @user.id)
       render json: {
-               status: 200,
-               message: 'User logged in successfully.',
-               user: {
-                 id: @user.id,
-                 username: @user.username,
-                 authentication_token: @user.authentication_token
-               }
-             },
-             status: :ok
+        message: 'User logged in successfully.',
+        username: @user.username,
+        token:
+      }, status: :ok
     else
-      render json: { status: 404, message: 'User not found.' }, status: :not_found
+      render json: { error: 'unauthorized' }, status: :unauthorized
     end
   end
+
+  private
+
+  def user_params
+    params.permit(:username)
+  end
 end
-
-# How to use this controller:
-#
-# 1. Make a POST request to http://localhost:3000/api/v1/sessions with a JSON body
-# containing the username of the user you want to log in.
-# 2. If the user is found, you will receive a JSON response with a status code of 200
-# and a message of "User logged in successfully.".
-# 3. If the user is not found, you will receive a JSON response with a
-# status code of 404 and a message of "User not found.".
-
-# How to effectively communicate with this controller:
-#
-# Your React component for user login
-# const handleLogin = async (username) => {
-#   try {
-#     const response = await fetch('http://localhost:3000/sessions', {
-#       method: 'POST',
-#       headers: {
-#         'Content-Type': 'application/json',
-#       },
-#       body: JSON.stringify({ username }),
-#     });
-
-#     if (response.ok) {
-#       const data = await response.json();
-#       console.log(data); // Check the response for success
-#     } else {
-#       const errorData = await response.json();
-#       console.error(errorData); // Check the response for errors
-#     }
-#   } catch (error) {
-#     console.error('An error occurred during login:', error);
-#   }
-# };
