e8328d5fc02ff94e9b91fed3b5558f2a363a2919
zilola <zilolanazarova1994@gmail.com>
2023-12-04 16:36:50 +0500

    Remove static token from database (we'll upgrade to dynamic tokens generated by jwt)

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,6 @@ class User < ApplicationRecord
   has_many :reservations, dependent: :destroy # only works if reservation model setup accurately.
   # has_many :workspaces, through: :reservations # only works if the workspace model setup accurately.
 
-  has_secure_token :authentication_token
-  # when a user is created, a token is created for them in the database
-
   # username must be unique and present in order to create a user and not null
   validates :username, presence: true, uniqueness: true, allow_blank: false
 end
