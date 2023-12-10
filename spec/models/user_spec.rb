8b71f1d069435bb0adfb36b9cbf5e17137216190
Alfred M <alfred.mkg@proton.me>
2023-12-10 05:35:08 +0300

    Add workspace model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'validations' do
-    it 'has to be present' do
+    it 'has to have a username' do
       user = User.create(username: nil)
       expect(user).not_to be_valid
     end
 
-    it 'has to be unique' do
+    it 'has to have a unique username' do
       User.create(username: 'test')
       user = User.new(username: 'test')
       expect(user).not_to be_valid
     end
 
-    it 'cannot be blank' do
+    it 'cannot have a blank username' do
       user = User.create(username: '')
       expect(user).not_to be_valid
     end
   end
 
   describe 'associations' do
-    it 'has many  posts' do
+    it 'has many reservations' do
       user = User.reflect_on_association(:reservations)
       expect(user.macro).to eq(:has_many)
     end
---
+++ spec/models/workspace_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe Workspace, type: :model do
+  let(:image) { fixture_file_upload('app/assets/images/pizza meme.jpg', 'image/jpg') }
+
+  describe 'validations' do
+    describe 'validates the name' do
+      it 'has to be present' do
+        workspace = Workspace.create(name: nil, description: 'testing the model', image:)
+        expect(workspace).not_to be_valid
+      end
+
+      it 'has to be unique' do
+        Workspace.create(name: 'name', description: 'testing the model', image:)
+        workspace = Workspace.new(name: 'name', description: 'this one is not unique', image:)
+        expect(workspace).not_to be_valid
+      end
+
+      it 'cannot be blank' do
+        workspace = Workspace.create(name: '', description: 'testing the model', image:)
+        expect(workspace).not_to be_valid
+      end
+    end
+
+    describe 'validates the description' do
+      it 'has to be present' do
+        workspace = Workspace.create(name: 'name', description: nil, image:)
+        expect(workspace).not_to be_valid
+      end
+
+      it 'cannot be blank' do
+        workspace = Workspace.create(name: 'name', description: '', image:)
+        expect(workspace).not_to be_valid
+      end
+    end
+
+    it 'must have an image' do
+      workspace = Workspace.create(name: 'name', description: 'testing the model', image: nil)
+      expect(workspace).not_to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'has many reservations' do
+      workspace = Workspace.reflect_on_association(:reservations)
+      expect(workspace.macro).to eq(:has_many)
+    end
+
+    it 'has one attached image' do
+      workspace = Workspace.create(name: 'name', description: 'testing the model', image:)
+      expect(workspace).to respond_to(:image)
+      expect(workspace.image).to be_an_instance_of(ActiveStorage::Attached::One)
+    end
+  end
+
+  it 'returns the image URL' do
+    workspace = Workspace.create(name: 'name', description: 'testing the model', image:)
+    expect(URI.parse(workspace.image_url)).to be_a(URI::HTTP)
+    expect { URI.parse(workspace.image_url) }.not_to raise_error
+  end
+end
