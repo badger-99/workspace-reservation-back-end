2fb5be45e288cbe3a53a066660f838ff6c372315
zilola <zilolanazarova1994@gmail.com>
2023-12-11 15:05:44 +0500

    Fix bug: async code running in image upload feature (ActiveStorage)

--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Reservation, type: :model do
-  let(:user) { User.create(username: 'robot') }
-  let(:image) { fixture_file_upload('app/assets/images/pizza meme.jpg', 'image/jpg') }
-  let(:workspace) { Workspace.create(name: 'name', description: 'testing the model', image:) }
-  let(:reservation) {
+  before :all do
+    @user = User.create(username: 'robot')
+    image = fixture_file_upload('app/assets/images/pizza meme.jpg', 'image/jpg')
+    @workspace = Workspace.create(name: 'name', description: 'testing the model', image:)
     Reservation.create(city: 'Reykjavik', start_date: Date.today, end_date: Date.today,
-                       user_id: user.id, workspace_id: workspace.id)
-  }
+                       user_id: @user.id, workspace_id: @workspace.id)
+  end
 
   describe 'associations' do
     it 'belongs to a user' do
@@ -25,13 +25,13 @@ RSpec.describe Reservation, type: :model do
     describe 'validates the start date' do
       it 'should be present' do
         reservation = Reservation.new(city: 'Reykjavik', start_date: nil, end_date: Date.today,
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
 
       it 'cannot be blank' do
         reservation = Reservation.new(city: 'Copenhagen', start_date: '', end_date: Date.today,
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
     end
@@ -39,13 +39,13 @@ RSpec.describe Reservation, type: :model do
     describe 'validates the end date' do
       it 'should be present' do
         reservation = Reservation.new(city: 'Oslo', start_date: Date.today, end_date: nil,
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
 
       it 'cannot be blank' do
         reservation = Reservation.new(city: 'Vienna', start_date: Date.today, end_date: '',
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
     end
@@ -53,13 +53,13 @@ RSpec.describe Reservation, type: :model do
     describe 'validates the city' do
       it 'should be present' do
         reservation = Reservation.new(city: nil, start_date: Date.today, end_date: Date.today,
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
 
       it 'cannot be blank' do
         reservation = Reservation.new(city: '', start_date: Date.today, end_date: Date.today,
-                                      user_id: user.id, workspace_id: workspace.id)
+                                      user_id: @user.id, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
     end
@@ -67,13 +67,13 @@ RSpec.describe Reservation, type: :model do
     describe 'validates the workspace' do
       it 'should be present' do
         reservation = Reservation.new(city: 'Stockholm', start_date: Date.today, end_date: Date.today,
-                                      user_id: user.id, workspace_id: nil)
+                                      user_id: @user.id, workspace_id: nil)
         expect(reservation).not_to be_valid
       end
 
       it 'cannot be blank' do
         reservation = Reservation.new(city: 'Tokyo', start_date: Date.today, end_date: Date.today,
-                                      user_id: user.id, workspace_id: '')
+                                      user_id: @user.id, workspace_id: '')
         expect(reservation).not_to be_valid
       end
     end
@@ -81,13 +81,13 @@ RSpec.describe Reservation, type: :model do
     describe 'validates the user' do
       it 'should be present' do
         reservation = Reservation.new(city: 'Nicosia', start_date: Date.today, end_date: Date.today,
-                                      user_id: nil, workspace_id: workspace.id)
+                                      user_id: nil, workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
 
       it 'cannot be blank' do
         reservation = Reservation.new(city: 'Miami', start_date: Date.today, end_date: Date.today,
-                                      user_id: '', workspace_id: workspace.id)
+                                      user_id: '', workspace_id: @workspace.id)
         expect(reservation).not_to be_valid
       end
     end
@@ -95,11 +95,9 @@ RSpec.describe Reservation, type: :model do
     describe 'validates uniqueness' do
       it 'A workspace cannot be reserved twice in the same time and city' do
         Reservation.create(start_date: Date.today, end_date: Date.new(2023, 12, 23),
-                           city: 'Dallas', workspace_id: workspace.id, user_id: user.id)
-
+                           city: 'Dallas', workspace_id: @workspace.id, user_id: @user.id)
         reservation = Reservation.new(start_date: Date.today, end_date: Date.new(2023, 12, 23),
-                                      city: 'Dallas', workspace_id: workspace.id, user_id: user.id)
-
+                                      city: 'Dallas', workspace_id: @workspace.id, user_id: @user.id)
         expect(reservation).not_to be_valid
       end
     end
