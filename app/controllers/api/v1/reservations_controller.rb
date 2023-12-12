ea65491d661b53974ba37d63db905e0d2da45607
Javier <javidx@gmail.com>
2023-12-12 20:04:32 +0200

    Added validation to the reservation create method to ensure that the users are not booking a past date or end dates that are earlier than the starting date of the reservation

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -15,6 +15,14 @@ class Api::V1::ReservationsController < Api::V1::ApplicationController
 
   def create
     @workspace = Workspace.find(params[:workspace])
+    if params[:start_date].to_date < Date.today || params[:end_date].to_date < Date.today
+      render json: { errors: 'The selected dates cannot be in the past.' }, status: :bad_request
+      return
+    end
+    if params[:start_date].to_date > params[:end_date].to_date
+      render json: { errors: 'The start date cannot be after the end date.' }, status: :bad_request
+      return
+    end
     @reservation = Reservation.new(reservation_params)
     @reservation.user_id = @current_user.id
     @reservation.workspace = @workspace
